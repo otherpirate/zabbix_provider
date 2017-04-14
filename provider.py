b210c4b22e072df6e0f1b7361236f5d6c1a6b577
otherpirate <mauro_murari@hotmail.com>
2017-04-14 15:10:13 -0300

    Creating methods to enabled and disabled hosts (+ List hosts disabled)

---
+++ provider.py
@@ -0,0 +1,27 @@
+from pyzabbix import ZabbixAPI
+from settings import ZABBIX_ENDPOINT, ZABBIX_USER, ZABBIX_PASSWORD
+
+STATUS_ENABLED = 0
+STATUS_DISABLED = 1
+
+
+class ZabbixProvider(object):
+    def __init__(self):
+        self.api = ZabbixAPI(ZABBIX_ENDPOINT)
+        self.api.login(ZABBIX_USER, ZABBIX_PASSWORD)
+
+    def hosts_disabled(self, groups=None):
+        return self.api.host.get(
+            output=['hostid', 'name'],
+            groupids=groups,
+            filter={'status': STATUS_DISABLED},
+        )
+
+    def enable_host(self, host_id):
+        return self.api.host.update(hostid=host_id, status=STATUS_ENABLED)
+
+    def disable_host(self, host_id):
+        return self.api.host.update(hostid=host_id, status=STATUS_DISABLED)
+
+    def __del__(self):
+        self.api.user.logout()
---
+++ settings.py
@@ -0,0 +1,5 @@
+import os
+
+ZABBIX_ENDPOINT = os.getenv("ZABBIX_ENDPOINT")
+ZABBIX_USER = os.getenv("ZABBIX_USER")
+ZABBIX_PASSWORD = os.getenv("ZABBIX_PASSWORD")
